c9692dfe330c1dfdbcf40710e4304c3d5da8f226
dpazmino94 <davidsebastianpc@hotmail.com>
2021-06-09 11:51:56 -0500

    AP 001 | Adding home page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../homepage/homepage.styles.scss' ;
+
+const HomePage = () => (
+  <div className='homepage'>
+      Homepage works!
+  </div>
+);
+
+export default HomePage;
